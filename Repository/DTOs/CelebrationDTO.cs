5602843b74bae5cd51f15e8326bd83168c54683b
Marlon Regis Amaral Pinto da Silva <mr.silva@samsung.com>
2022-06-06 16:07:51 -0300

    Create a database and repository

---
+++ Repository/DTOs/CelebrationDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DTOs
+{
+    public  class CelebrationDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public DateTime RecordDate { get; set; }
+        public DateTime CelebrationDate { get; set; }
+    }
+}
---
+++ Repository/DbContexts/CelebrationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.DTOs;
+
+namespace Repository.DbContexts
+
+{
+    public class CelebrationDbContext : DbContext
+    {
+
+        public DbSet<CelebrationDTO> Reservations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       => optionsBuilder.UseSqlite("Data Source=c:/Sidi/study/FinalProject/CelebrationApp/Repository/celebration.db");
+    }
+}
