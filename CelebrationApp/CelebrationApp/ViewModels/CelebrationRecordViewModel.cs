df3f8924512395f2126341b7ad80be41eff1055e
Salomão Brito de Luna <salomao.bl@samsung.com>
2022-06-11 14:21:24 -0300

    Create List Celebration on ListView(View and ViewModel)

---
+++ CelebrationApp/CelebrationApp/ViewModels/CelebrationRecordViewModel.cs
@@ -0,0 +1,64 @@
+﻿using CelebrationApp.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelebrationApp.ViewModels
+{
+    public class CelebrationRecordViewModel : ObservableObject
+    {
+        public Guid Id { get; set; }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        private DateTime celebrationDate;
+        public DateTime CelebrationDate
+        {
+            get { return celebrationDate; }
+            set { SetProperty(ref celebrationDate, value); }
+        }
+
+        private DateTime registerDate;
+        public DateTime RegisterDate
+        {
+            get { return registerDate; }
+            set { SetProperty(ref registerDate, value); }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { SetProperty(ref description, value); }
+        }
+
+        private string image;
+
+        public string Image
+        {
+            get { return image; }
+            set { SetProperty(ref image, value); }
+        }
+
+        public CelebrationRecordViewModel(Celebration celebration)
+        {
+            Id = celebration.id;
+            Name = celebration.Name;
+            CelebrationDate = celebration.CelebrationDate;
+            RegisterDate = celebration.RecordDate;
+            Description = celebration.Description;
+            Image = "ms-appx:///Assets/Images/Gift.png";
+
+        }
+
+
+    }
+}
