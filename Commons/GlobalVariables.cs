062ae8cbb4aac57ab326527213d4e06f23249ed7
Marlon Regis <marlonregis14@outlook.com>
2022-07-05 18:02:35 -0300

    Fix: error about testeGitVersion resolved

--- Commons/GlobalVariables.cs
+++ Commons/GlobalVariables.cs
@@ -11,7 +11,7 @@ namespace Commons
 
         public const string DataBaseString = "C:/Sidi/study/FinalProject/CelebrationApp/Repository/celebration.db";
         public const string FolderLogString = "C:/Sidi/study/FinalProject/CelebrationApp/Repository/celebration.db";
-        public const string testeGitVersion = "C:/Sidi/study/FinalProject/CelebrationApp/Repository/celebration.db";
+        public const string testeGitVersion = "/Repository/celebration.db";
     }
 }
 
